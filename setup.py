2de31f5b506cf315de4b4c1f5ff4bda7f6198e50
SPChan <shihping.chan@gmail.com>
2021-05-16 20:59:42 +0800

    fix dict_items is not a list in Python 3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ requirements = [x.strip() for x in open("requirements.txt", "r").readlines()]
 
 setup(
     name = "sippy",
-    version = "2.0.dev0",
+    version = "2.0.dev1",
     packages = find_packages(),
 
     install_requires = requirements,
